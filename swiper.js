f811d6d99864bfa45ed838504a002735f01a7b97
Ryota <143061672+Ry0t7@users.noreply.github.com>
2023-12-28 19:33:31 +0900

    Create swiper.js

---
+++ swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiperApparel = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
